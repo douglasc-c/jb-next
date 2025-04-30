492fcbb0982b3b5df424751d99d16b095c00d645
Douglas Camargo <douglas.camargo.ltda@gmail.com>
2025-04-30 16:18:12 -0300

    fix(website):change text of website

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -35,7 +35,7 @@ export default function HomePage() {
       </a>
 
       {/* Hero Section */}
-      <section className="bg-card bg-cover bg-center py-32 relative overflow-hidden">
+      <section className="bg-card bg-cover bg-center py-32 relative overflow-hidden h-screen flex items-center justify-center">
         {/* Background Image */}
         <div className="absolute inset-0 bg-gradient-to-r from-zinc-900/95 to-zinc-950/65 z-0"></div>
 
@@ -298,7 +298,7 @@ export default function HomePage() {
                 whileInView={{ opacity: 1, x: 0 }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.8, delay: 0.2 }}
-                className="relative h-[300px] rounded-lg overflow-hidden shadow-2xl mt-8"
+                className="relative h-[320px] rounded-lg overflow-hidden shadow-2xl mt-8"
               >
                 <Image
                   src="/images/bg/table-graph.jpeg"
@@ -693,14 +693,12 @@ export default function HomePage() {
                     value: t('contact.whatsapp.value'),
                     link: whatsappUrl,
                     icon: (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 text-[#e79204]"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824z" />
-                      </svg>
+                      <Image
+                        src="/images/svg/whatsapp.svg"
+                        width={26}
+                        height={26}
+                        alt="Document"
+                      />
                     ),
                   },
                   {
