ed5d0b20f6954ad2cf02b73adf9799532a1ac181
Douglas Camargo <douglas.camargo.ltda@gmail.com>
2025-05-06 11:34:06 -0300

    fix(api):change production

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://auditaxs.com.br',
   timeout: 10000, // 10 segundos
   withCredentials: true,
   headers: {
